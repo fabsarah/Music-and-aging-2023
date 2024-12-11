018d197012ec230f4f8286dced8749360a32f6b4
Sarah Faber <31863074+fabsarah@users.noreply.github.com>
2024-12-11 12:01:26 -0400

    Update 3_FO_PLS.m
    Improved comments, removed redundancies

--- 3_FO_PLS.m
+++ 3_FO_PLS.m
@@ -1,149 +1,47 @@
 %% Now, let's start the analysis!
-% This picks up where step 1 (HMM_Estimation) left off. Most of the data here is from the 
-% structure array created in that step. If you're using this from "scratch", I've tried to 
-% give descriptions throughout of what the matrices are/their dimensionality
+% This picks up where step 2 (testing the Ks) left off, and most of the data here is from the 
+% structure array created in step 1 (HMM Estimation) using our "winning" K value. 
 % 
+% We could use the PLS results from our K tests, but I've shown the non-looped PLS
+% here for posterity. Delicious, delicious posterity!
+%
+% If you're using this from "scratch", I've tried to 
+% give descriptions throughout of what the matrices are/their dimensionality
 %
 % First up, we're going to use the block_dex variable (tells us which block is
 % which for stimuli, ratings, etc) to mke some new matrices.
 
-tempages = Age; % this is a matrix with participants' ages 
-fMRI_hmm4_music.Ages = tempages; %save it to the array
+%% Compare folks who have clean pre- and post-intervention data
+% these are indices matching up the participants' data pre- and post-intervention
+match_pre = [1;3;5;8;10;9;11;14;15;21;26;27;29;33;37];
+match_post = [2;4;7;12;13;16;20;23;24;25;28;30;32;35;38];
 
-%%
-tempdata = presentation_table; % this is a table of stimulus triggers for all participants
-block_dex = cell(80,1);% 80 participants
-start = 1;% initialize a counter, stimulus-wise
-stop = start+23;% 24 stimuli for each
-for part = 1:80
-    tempdex = tempdata(start:stop,:);% go through the data 
-    block_dex{part} = tempdex;% get the values, add them to the new array
-    start = stop+1;% get the new start position
-    stop = start+23;% and the new stop position
-end
-clear start stop part tempdex
-fMRI_hmm4_music.block_dex = block_dex; %add it to the array
+tempdata = fMRI_hmm4;
+music_pre = tempdata.music_FO(match_pre,:);
+music_post = tempdata.music_FO(match_post,:);
 
-%% And now use this index to organize the FO matrices:
-tempdata = fMRI_hmm4_music;
-stim_FO = cell(80,3);% 80 participants, three stimulus types
-for part = 1:80
-    index = tempdata.block_dex{part};% get the participant's stimulus trigger order
-    block = index(:,2);% for me, this information was stored in the second column
-    tempFO = tempdata.block_FO{part};% get the participant's FO data
-    for stim = 1:3 % 3 stimulus types
-        newmat = tempFO(block==stim,:);% get the relevant FO
-        stim_FO{part,stim} = newmat;%put it into the new matrix
-    end
-end
-fMRI_hmm4_music.stim_FO = stim_FO;% save it to the array!
-clear part index block tempFO stim newmat %make look nice
-
-%% Now, average and assemble into three more matrices
-% stim_FO gives us a participant*stimulus category cell array, so now let's do some 
-% averaging so we can run PLS:
-
-K = fMRI_hmm4_music.options.K;
-tempdata = fMRI_hmm4_music.stim_FO;
-stim_FO_all = cell(3,1);
-for stimcat = 1:3 % stimulus category
-    stimmat = nan(80,K);
-    for part = 1:80
-        partv = tempdata{part,stimcat}; %get the data
-        if isempty(partv)
-            continue
-        else
-                tempFO = nanmean(partv);% average it down
-                stimmat(part,:) = tempFO; %put it in the temporary matrix
-        end
-    end
-    stim_FO_all{stimcat} = stimmat;% add the matrix to the new cell array
-end
-fMRI_hmm4_music.stim_FO_all = stim_FO_all;% save it to the array
-clear stimcat part stimmat part partv state tempFO % make look nice
-
-%% PLS time!
+%% PLS Time!
 addpath(genpath('Pls'))
-clear option % make sure there isn't an old option file kicking around
-indata_stim = cell2mat(stim_FO_all);% set our input data. We should have a task*1 cell array where each cell is a participant*K matrix of FO values
-indata_stim(isnan(indata_stim))=0;% make sure there are no nans
-option.method = 1;% mean-centred PLS
+clear option % make sure there isn't an old options file hanging around
+
+
+indata = {[match_pre;match_post]};
+
+option.method = 1;
 option.num_perm = 500;
 option.num_boot = 100;
-nparts = 80;% 80 participants
-ntask = 3;% 3 tasks/stimulus categories
+nparts = length(match_pre);% how many participants in the analysis?
+nconds = 2;%2 conditions, pre- and post-intervention
 
-stim_res = pls_analysis({indata_stim},nparts,ntask,option); % run the analysis! 
+match_res = pls_analysis(indata,nparts,nconds,option);% run the analysis
 
-%% Plot the LVs!
-res = stim_res;
-LV = 1;
-p = res.perm_result.sprob(LV);% can check this whole array to see how many significant LVs you have. It works like a traditional p value
-K = fMRI_hmm4_music.options.K;
-
-figure
-subplot(1,2,1)
-z = res.boot_result.orig_usc(:,LV);% get the usc matrix. This shows us the "contrast" between tasks/stimuli types
-bar(z)
-hold on
-yneg = res.boot_result.llusc(:,LV);
-ypos = res.boot_result.ulusc(:,LV);
-errorbar(1:length(z),z,yneg-z,ypos-z,'.')
-xticklabels({'Self-Select';'Popular';'Novel'})
-xtickangle(45)
-grid on
-title(sprintf('PLS Stimuli: All Ages, p = %d',p),'FontSize',12)
-
-x = res.boot_result.compare_u(:,LV);
-x = reshape(x,K,[]);
-%x(abs(x)<6) = 0;% can threshold this for readability 
-subplot(1,2,2)
-pbaspect([1 1 1])
-shower_tile_plot(x);%imagesc will also work, just not as "crisp"
-xticks([])
-yticks([])
-ylabel('States')
-colorbar
-title(sprintf('LV %d',LV),'FontSize',12)
-
-clear res K LV p x y* z
-%% Now...is there an age effect?
-% Here, we'll divide up our matrix into age groups:
-Age_stim = cell(3,2);% pre-allocate a new cell
-idx_old = fMRI_hmm4_music.Ages>40;% sorry :(
-idx_young = fMRI_hmm4_music.Ages<40;
-for i = 1:3
-    tempmat = fMRI_hmm4_music.stim_FO_all{i};% get the FO data
-    Age_stim{i,1} = tempmat(idx_young==1,:);separate into separate cells
-    Age_stim{i,2} = tempmat(idx_old==1,:);
-end
-clear i
-%%
-%addpath(genpath('Pls'));
-clear option
-indata{1,1} = cell2mat(Age_stim(:,1));
-indata{2,1} = cell2mat(Age_stim(:,2));
-for i = 1:length(indata) % make sure there are no nans
-    tempdata = indata{i,:};
-    tempdata(isnan(tempdata)) = 0;
-    indata{i,:} = tempdata;
-end
-
-option.method = 1;% mean-centred PLS
-option.num_perm = 500;
-option.num_boot = 100;
-nparts = [length(indata{1});length(indata{2})];% we now have different group lengths
-ntask = 3;
-
-all_res = pls_analysis(indata,{nparts},ntask,option);
-%young_res = pls_analysis({indata{1}},nparts(1),ntask,option);
-%old_res = pls_analysis({indata{2}},nparts(2),ntask,option);
-
-%% Go up a block for the syntax to plot young and old separately. Stay here to plot everyone together:
-res = all_res;
+%% Plotting
+res = match_res;
+K = 4;% the K value for the estimation
 LV = 1;
 p = res.perm_result.sprob(LV);
-K = fMRI_hmm4_music.options.K;
+short_labs = {'Pre';'Post'};
+heading = sprintf('PLS Pre-Post: Music, p = %d',p);
 
 figure
 subplot(1,2,1)
@@ -154,15 +52,14 @@ yneg = res.boot_result.llusc(:,LV);
 ypos = res.boot_result.ulusc(:,LV);
 errorbar(1:length(z),z,yneg-z,ypos-z,'.')
 colorbar off
-xticklabels({'Self-Select Y';'Popular Y';'Novel Y';...
-    'Self-Select O';'Popular O';'Novel O'})
+xticklabels(short_labs)
 xtickangle(45)
 grid on
-title(sprintf('PLS Stimuli: Age and Stimuli, p = %d',p),'FontSize',12)
+title(heading,'FontSize',12)
 
 x = res.boot_result.compare_u(:,LV);
 x = reshape(x,K,[]);
-%x(abs(x)<6) = 0;
+%x(abs(x)<3) = 0;% the result can be thresholded for readability
 subplot(1,2,2)
 pbaspect([1 1 1])
 shower_tile_plot(x);
@@ -171,53 +68,119 @@ yticks([])
 ylabel('States')
 colorbar
 title(sprintf('LV %d',LV),'FontSize',12)
-clear z y* x K res LV p
 
-%% Save the results!
-fMRI_hmm4_music.FOPLS.input = Age_stim;
-fMRI_hmm4_music.FOPLS.results.general = stim_res;
-fMRI_hmm4_music.FOPLS.results.Young = young_res;
-fMRI_hmm4_music.FOPLS.results.Old = old_res;
-fMRI_hmm4_music.FOPLS.results.all = all_res;
+clear x y z p yneg ypos LV res ans option numsubs numcond heading
 
-%% Plot the input data all together:
-K = fMRI_hmm4_music.options.K;
-ntask = 3;
+%% Now the correlation analyses:
+% For the next part of the analysis, I'm using correlation matrices I extracted
+% between behavioural liking and familiarity data, and the HMM measures
+
+tempdata = fMRI_hmm4_music.LF_PLS;
+like_pre = tempdata.Liking.like_mat(match_pre,:);
+like_post = tempdata.Liking.like_mat(match_post,:);
+fam_pre = tempdata.Fam.fam_mat(match_pre,:);
+fam_post = tempdata.Fam.fam_mat(match_post,:);
+
+%% PLS Time!
+%addpath(genpath('Pls'))
+% Here, I want to look for intervention differences in liking and familiarity
+% correlations
+
+clear option
+
+indata_like = {[like_pre;like_post]};
+indata_fam = {[fam_pre;fam_post]};
+indata_all = {[like_pre;fam_pre;like_post;fam_post]};
+
+option.method = 1;
+option.num_perm = 500;
+option.num_boot = 100;
+nparts = 15;% number of participants
+nconds = 2;% number of conditions
+
+match_like = pls_analysis(indata_like,nparts,nconds,option);
+match_fam = pls_analysis(indata_fam,nparts,nconds,option);
+match_likefam = pls_analysis(indata_all,nparts,nconds*2,option);%nconds here is 4 because we're looking at 2 metrics*2 conditions
+clear option indata*
+
+%% Plotting
+res = match_likefam;
+LV = 1;
+K = 4;
+p = res.perm_result.sprob(LV);
+short_labs = {'Pre';'Post'};
+mvr_labs = {'Like Pre';'Fam Pre';'Like Post';'Fam Post'};
+heading = sprintf('PLS Pre-Post: Like & Fam, p = %d',p);
 
 figure
-stim_labs = {'Self-Select';'Popular';'Novel'};
-for i = 1:3
-    subplot(1,ntask,i)
-    imagesc(stim_FO_all{i})
-    %caxis([0 50])
-    ylabel('Participants')
-    xticks(1:K)
-    xlabel('State')
-    colorbar
-    title(stim_labs{i},'FontSize',12)
-end
-clear i
+subplot(1,2,1)
+z = res.boot_result.orig_usc(:,LV);
+bar(z)
+hold on
+yneg = res.boot_result.llusc(:,LV);
+ypos = res.boot_result.ulusc(:,LV);
+errorbar(1:length(z),z,yneg-z,ypos-z,'.')
+colorbar off
+xticklabels(mvr_labs)
+xtickangle(45)
+grid on
+title(heading,'FontSize',12)
 
-%% Plot the input data by age:
+x = res.boot_result.compare_u(:,LV);
+x = reshape(x,4,[]);
+%x(abs(x)<3) = 0;
+subplot(1,2,2)
+pbaspect([1 1 1])
+shower_tile_plot(x);
+xticks([])
+yticks([])
+ylabel('States')
+colorbar
+title(sprintf('LV %d',LV),'FontSize',12)
+
+clear x y z p yneg ypos LV res ans option numsubs numcond heading
+
+%% Plot the indata:
 figure
-stim_labs = {'Self-Select';'Popular';'Novel'};
-for i = 1:3
-    subplot(1,3,i)
-    imagesc(Age_stim{i,1})
-    %caxis([0 50])
-    ylabel('Participants')
-    xticks(1:5)
-    xlabel('State')
-    colorbar
-    title(strcat(stim_labs{i},' Younger Adults'),'FontSize',12)
+subplot(2,2,1)
+imagesc(like_pre)
+colorbar
+title('Liking Pre','FontSize',12)
 
-    subplot(1,3,i+3)
-    imagesc(Age_stim{i,2})
-    %caxis([0 50])
-    ylabel('Participants')
-    xticks(1:5)
-    xlabel('State')
-    colorbar
-    title(strcat(stim_labs{i},' Older Adults'),'FontSize',12)
-end
-clear i
+subplot(2,2,2)
+imagesc(like_post)
+colorbar
+title('Liking Post','FontSize',12)
+
+subplot(2,2,3)
+imagesc(fam_pre)
+colorbar
+title('Familiarity Pre','FontSize',12)
+
+subplot(2,2,4)
+imagesc(fam_post)
+colorbar
+title('Familiarity Post','FontSize',12)
+
+%% Save it all:
+match_PLS.FO.Input.rest_pre = rest_pre;
+match_PLS.FO.Input.rest_post = rest_post;
+match_PLS.FO.Input.music_pre = music_pre;
+match_PLS.FO.Input.music_post = music_post;
+match_PLS.FO.Results.match_rest_res = match_rest_res;
+match_PLS.FO.Results.match_music_res = match_music_res;
+match_PLS.FO.Results.match_mvr_res = match_mvr_res;
+match_PLS.FO.Results.match_stim_res = match_stim_res;
+match_PLS.FO.Results.match_rest_stim_res =match_rest_stim_res;
+
+match_PLS.corr.Input.like_pre = like_pre;
+match_PLS.corr.Input.like_post = like_post;
+match_PLS.corr.Input.fam_pre = fam_pre;
+match_PLS.corr.Input.fam_post = fam_post;
+match_PLS.corr.Results.match_like_res = match_like;
+match_PLS.corr.Results.match_fam_res = match_fam;
+match_PLS.corr.Results.match_likefam_res = match_likefam;
+
+fMRI_hmm4_music.match_PLS.match_dex.match_pre = match_pre;
+fMRI_hmm4_music.match_PLS.match_dex.match_post = match_post;
+fMRI_hmm4_music.match_PLS = match_PLS;
